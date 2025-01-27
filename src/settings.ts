4fbad110afc360af3858d0f3cba36a4263468862
Per Slycke <6725402+slycke@users.noreply.github.com>
2025-01-27 22:00:26 +0100

    cleanup name

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'EPEXMonitor';
 /**
  * This must match the name of your plugin as defined the package.json `name` property
  */
-export const PLUGIN_NAME = 'homebridge-epex';
+export const PLUGIN_NAME = 'homebridge-epex-energy-price';
